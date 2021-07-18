f1933e745ea4966e2bdd26a151fc66d848c89359
unknown <noman.shaikh49@yahoo.com>
2021-07-18 17:57:50 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//Route::get('/',[App\Http\Controllers\EmailFetchController::class,'index']);
+Route::get('/',[App\Http\Controllers\EmailFetchController::class,'connectPage']);
+Route::post('connect',[App\Http\Controllers\EmailFetchController::class,'index'])->name('connect');
+Route::get('emails', function () {
+        return view('emails');
+    })->name('emails');
+Route::post('search',[App\Http\Controllers\EmailFetchController::class,'custom_search'])->name('search');
+    
+//Route::get('indexPage',[App\Http\Controllers\EmailFetchController::class,'index'])->name('indexPage');
